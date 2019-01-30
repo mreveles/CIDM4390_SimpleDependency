7fc0a7b108f56dd4f446a178e13fea10dfd9c0cb
mreveles <mareveles1@buffs.wtamu.edu>
2019-01-30 16:11:23 -0600

    First Commit

---
+++ MessagePrinter/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace MessagePrinter
+{
+    public class MessagePrintingService
+    {
+        public string Message {get;set;} = "Default";
+
+        public MessagePrintingService()
+        {
+
+        }
+
+        public MessagePrintingService(string message)
+        {
+            this.Message = message;
+        }
+        
+        public void PrintMessage()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SimpleDependency/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MessagePrinter;
+
+namespace SimpleDependency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new MessagePrintingService();
+
+            service.PrintMessage();
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+            
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
